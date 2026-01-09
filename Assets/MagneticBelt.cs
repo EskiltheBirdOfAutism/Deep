6e5f753c7a473ce69b875432b4f540996bcce15a
SiggeSolders <sigge.hallquistsolders@elev.ga.lbs.se>
2026-01-09 12:09:43 +0100

    bälte

---
+++ Assets/MagneticBelt.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class MagneticBelt : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
