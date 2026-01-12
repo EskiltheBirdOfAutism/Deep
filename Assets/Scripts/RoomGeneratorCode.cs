5c07be0df58818f21c016d298713f6112108d413
EskiltheBirdOfAutism <154226055+EskiltheBirdOfAutism@users.noreply.github.com>
2026-01-12 21:57:26 +0100

    cleaning

--- Assets/Scripts/RoomGeneratorCode.cs
+++ Assets/Scripts/RoomGeneratorCode.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Unity.Netcode;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -15,110 +16,113 @@ public class RoomGeneratorCode : NetworkBehaviour
     private bool room_change = false;
     private bool room_change_previous = false;
     private GameObject[] room_id = new GameObject[65];
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
+    void Start()
     {
-        for (int _i = 0; _i < 64; _i++)
+        if (OwnerClientId == 0)
         {
-            room_pos[_i] = new Vector3(0, 0, 0);
-            room_change_previous = room_change;
-            room_change = false;
-            if (_i > 0)
+            for (int _i = 0; _i < 64; _i++)
             {
+                room_pos[_i] = new Vector3(0, 0, 0);
+                room_change_previous = room_change;
                 room_change = false;
-                room_pos[_i] = room_pos[_i - 1] + new Vector3(0, -25, 0);
-                if (Random.Range(0, 100) < 70 && room_change_previous == false)
+                if (_i > 0)
                 {
-                    float _offset = 1;
-                    if (Random.Range(0, 100) <= 50) _offset = -1;
+                    room_change = false;
+                    room_pos[_i] = room_pos[_i - 1] + new Vector3(0, -25, 0);
+                    if (Random.Range(0, 100) < 70 && room_change_previous == false)
+                    {
+                        float _offset = 1;
+                        if (Random.Range(0, 100) <= 50) _offset = -1;
 
-                    room_pos[_i] = room_pos[_i - 1] + new Vector3(_offset * 50, 0, 0);
-                    if(Random.Range(0, 100) <= 50) room_pos[_i] = room_pos[_i - 1] + new Vector3(0, 0, _offset * 50);
-                    room_change = true;
+                        room_pos[_i] = room_pos[_i - 1] + new Vector3(_offset * 50, 0, 0);
+                        if (Random.Range(0, 100) <= 50) room_pos[_i] = room_pos[_i - 1] + new Vector3(0, 0, _offset * 50);
+                        room_change = true;
+                    }
                 }
             }
-        }
 
-        for (int _i = 0; _i < 64; _i++)
-        {
-            if(_i > 0)
+            for (int _i = 0; _i < 64; _i++)
             {
-                if (room_pos[_i + 1].y == room_pos[_i].y)
+                if (_i > 0)
                 {
-                    GameObject _room = Instantiate(roomsideup, room_pos[_i], Quaternion.identity);
-                    _room.transform.rotation = Quaternion.Euler(0, 0, 0);
-                    if (room_pos[_i + 1].x - room_pos[_i].x < 0)
+                    if (room_pos[_i + 1].y == room_pos[_i].y)
                     {
-                        _room.transform.rotation = Quaternion.Euler(0, 180, 0);
-                    }
-                    if (room_pos[_i + 1].z - room_pos[_i].z < 0)
-                    {
-                        _room.transform.rotation = Quaternion.Euler(0, 90, 0);
-                    }
-                    if (room_pos[_i + 1].z - room_pos[_i].z > 0)
-                    {
-                        _room.transform.rotation = Quaternion.Euler(0, 270, 0);
-                    }
-                    _room.GetComponent<NetworkObject>().Spawn();
-                    room_id[_i] = _room.gameObject;
-                }
-                else
-                {
-                    if (room_pos[_i - 1].y > room_pos[_i].y)
-                    {
-                        GameObject _room = Instantiate(roomdownup, room_pos[_i], Quaternion.identity);
-                        _room.GetComponent<NetworkObject>().Spawn();
-                        room_id[_i] = _room.gameObject;
-                    }
-                    else
-                    {
-                        GameObject _room = Instantiate(roomsidedown, room_pos[_i], Quaternion.identity);
+                        GameObject _room = Instantiate(roomsideup, room_pos[_i], Quaternion.identity);
                         _room.transform.rotation = Quaternion.Euler(0, 0, 0);
-                        if (room_pos[_i - 1].x - room_pos[_i].x < 0)
+                        if (room_pos[_i + 1].x - room_pos[_i].x < 0)
                         {
                             _room.transform.rotation = Quaternion.Euler(0, 180, 0);
                         }
-                        if (room_pos[_i - 1].z - room_pos[_i].z < 0)
+                        if (room_pos[_i + 1].z - room_pos[_i].z < 0)
                         {
                             _room.transform.rotation = Quaternion.Euler(0, 90, 0);
                         }
-                        if (room_pos[_i - 1].z - room_pos[_i].z > 0)
+                        if (room_pos[_i + 1].z - room_pos[_i].z > 0)
                         {
                             _room.transform.rotation = Quaternion.Euler(0, 270, 0);
                         }
                         _room.GetComponent<NetworkObject>().Spawn();
                         room_id[_i] = _room.gameObject;
                     }
-                }
-            }
-            else
-            {
-                if (room_pos[_i + 1].y == room_pos[_i].y)
-                {
-                    GameObject _room = Instantiate(roomside, room_pos[_i], Quaternion.identity);
-                    _room.transform.rotation = Quaternion.Euler(0, 0, 0);
-                    if (room_pos[_i + 1].x - room_pos[_i].x < 0)
+                    else
                     {
-                        _room.transform.rotation = Quaternion.Euler(0, 180, 0);
+                        if (room_pos[_i - 1].y > room_pos[_i].y)
+                        {
+                            GameObject _room = Instantiate(roomdownup, room_pos[_i], Quaternion.identity);
+                            _room.GetComponent<NetworkObject>().Spawn();
+                            room_id[_i] = _room.gameObject;
+                        }
+                        else
+                        {
+                            GameObject _room = Instantiate(roomsidedown, room_pos[_i], Quaternion.identity);
+                            _room.transform.rotation = Quaternion.Euler(0, 0, 0);
+                            if (room_pos[_i - 1].x - room_pos[_i].x < 0)
+                            {
+                                _room.transform.rotation = Quaternion.Euler(0, 180, 0);
+                            }
+                            if (room_pos[_i - 1].z - room_pos[_i].z < 0)
+                            {
+                                _room.transform.rotation = Quaternion.Euler(0, 90, 0);
+                            }
+                            if (room_pos[_i - 1].z - room_pos[_i].z > 0)
+                            {
+                                _room.transform.rotation = Quaternion.Euler(0, 270, 0);
+                            }
+                            _room.GetComponent<NetworkObject>().Spawn();
+                            room_id[_i] = _room.gameObject;
+                        }
                     }
-                    if (room_pos[_i + 1].z - room_pos[_i].z < 0)
-                    {
-                        _room.transform.rotation = Quaternion.Euler(0, 90, 0);
-                    }
-                    if (room_pos[_i + 1].z - room_pos[_i].z > 0)
-                    {
-                        _room.transform.rotation = Quaternion.Euler(0, 270, 0);
-                    }
-                    _room.GetComponent<NetworkObject>().Spawn();
-                    room_id[_i] = _room.gameObject;
                 }
                 else
                 {
-                    GameObject _room = Instantiate(roomdown, room_pos[_i], Quaternion.identity);
-                    _room.GetComponent<NetworkObject>().Spawn();
-                    room_id[_i] = _room.gameObject;
+                    if (room_pos[_i + 1].y == room_pos[_i].y)
+                    {
+                        GameObject _room = Instantiate(roomside, room_pos[_i], Quaternion.identity);
+                        _room.transform.rotation = Quaternion.Euler(0, 0, 0);
+                        if (room_pos[_i + 1].x - room_pos[_i].x < 0)
+                        {
+                            _room.transform.rotation = Quaternion.Euler(0, 180, 0);
+                        }
+                        if (room_pos[_i + 1].z - room_pos[_i].z < 0)
+                        {
+                            _room.transform.rotation = Quaternion.Euler(0, 90, 0);
+                        }
+                        if (room_pos[_i + 1].z - room_pos[_i].z > 0)
+                        {
+                            _room.transform.rotation = Quaternion.Euler(0, 270, 0);
+                        }
+                        _room.GetComponent<NetworkObject>().Spawn();
+                        room_id[_i] = _room.gameObject;
+                    }
+                    else
+                    {
+                        GameObject _room = Instantiate(roomdown, room_pos[_i], Quaternion.identity);
+                        _room.GetComponent<NetworkObject>().Spawn();
+                        room_id[_i] = _room.gameObject;
+                    }
                 }
+                SendRoomIdServerRpc(room_id[_i].GetComponent<NetworkObject>(), _i);
             }
         }
     }
@@ -145,6 +149,26 @@ public class RoomGeneratorCode : NetworkBehaviour
                     }
                 }
             }
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SendRoomIdServerRpc(NetworkObjectReference _room_id, int _index)
+    {
+        UpdateClientRoomIdClientRpc(_room_id, _index);
+    }
+
+    [ClientRpc]
+    private void UpdateClientRoomIdClientRpc(NetworkObjectReference _room_id, int _index)
+    {
+        if(IsOwner && room_id[_index] == null)
+        {
+            if(!_room_id.TryGet(out NetworkObject _net_obj))
+            {
+                return;
+            }
+
+            room_id[_index] = _net_obj.gameObject;
         }
     }
 }
