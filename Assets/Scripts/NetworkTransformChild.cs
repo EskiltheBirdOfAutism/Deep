a1605a4fdb4d6e862a42d96271162f836f55c1a5
EskiltheBirdOfAutism <154226055+EskiltheBirdOfAutism@users.noreply.github.com>
2026-01-13 01:04:21 +0100

    cleaning

--- Assets/Scripts/NetworkTransformChild.cs
+++ Assets/Scripts/NetworkTransformChild.cs
@@ -7,7 +7,7 @@ public class NetworkTransformChild : NetworkBehaviour
     // Det här är varaibler som krävs för att skicka till children av parent
     // Vi har då en lista för alla transforms som vi ska ändra
     // namnen är lite fel, target_transform är de transforms vi ska ändra, medan target_position och target_rotation är vad det ska bli
-    [SerializeField] public Transform[] target_transform = new Transform[120];
+    [SerializeField] public GameObject[] target_object = new GameObject[120];
 
     public Vector3[] target_position = new Vector3[120];
     public Quaternion[] target_rotation = new Quaternion[120];
@@ -15,12 +15,12 @@ public class NetworkTransformChild : NetworkBehaviour
     private void Start()
     {
         // Vi sätter alla targets till vad de transforms vi har nu
-        for (int _i = 0; _i < target_transform.Length; _i++)
+        for (int _i = 0; _i < target_object.Length; _i++)
         {
-            if (target_transform[_i] != null)
+            if (target_object[_i] != null)
             {
-                target_position[_i] = target_transform[_i].localPosition;
-                target_rotation[_i] = target_transform[_i].localRotation;
+                target_position[_i] = target_object[_i].transform.localPosition;
+                target_rotation[_i] = target_object[_i].transform.localRotation;
             }
         }
 
@@ -39,32 +39,33 @@ public class NetworkTransformChild : NetworkBehaviour
         
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         // Vi kollar först ifall denna objekt tillhör ägaren
         if (IsOwner)
         {
             // Ifall det är så skickar vi positionerna och rotationerna
             // Vi gör det genom en for loop av alla childrens transforms, så skickar vi det då från klienten till servern
-            for (int _i = 0; _i < target_transform.Length; _i++)
+            Vector3[] _pos = new Vector3[target_object.Length];
+            Quaternion[] _rot = new Quaternion[target_object.Length];
+            for (int _i = 0; _i < target_object.Length; _i++)
             {
-                if (target_transform[_i] != null)
-                {
-                    SendTransformServerRpc(target_transform[_i].localPosition, target_transform[_i].localRotation, _i);
-                }
+                _pos[_i] = target_object[_i].transform.position;
+                _rot[_i] = target_object[_i].transform.rotation;
             }
+            SendTransformServerRpc(_pos, _rot);
         }
         else
         {
             // Ifall objektet inte tillhör ägaren ändrar vi då positionerna och rotationerna till vad de ska vara
             // Det funkar då alltså att klienten skickar sin information till servern baserat på sina egna positioner och rotationer
             // Sedan tar man då positionerna och rotationerna skickat från andra klienter, så ändrar vi deras represenativa objekts positioner till de positionerna och rotationer
-            for (int _i = 0; _i < target_transform.Length; _i++)
+            for (int _i = 0; _i < target_object.Length; _i++)
             {
-                if (target_transform[_i] != null)
-                { 
-                    target_transform[_i].localPosition = Vector3.Lerp(target_transform[_i].localPosition, target_position[_i], Time.fixedDeltaTime * 10f);
-                    target_transform[_i].localRotation = Quaternion.Slerp(target_transform[_i].localRotation, target_rotation[_i], Time.fixedDeltaTime * 10f);
+                if (target_object[_i] != null)
+                {
+                    target_object[_i].transform.localPosition = Vector3.Lerp(target_object[_i].transform.localPosition, target_object[_i].transform.position, Time.fixedDeltaTime * 10f);
+                    target_object[_i].transform.localRotation = Quaternion.Slerp(target_object[_i].transform.localRotation, target_object[_i].transform.rotation, Time.fixedDeltaTime * 10f);
                 }
             }
         }
@@ -72,22 +73,25 @@ public class NetworkTransformChild : NetworkBehaviour
 
     // Den här headern innebär att vi skickar information till servern
     [ServerRpc(RequireOwnership = false)]
-    private void SendTransformServerRpc(Vector3 _pos, Quaternion _rot, int _index)
+    private void SendTransformServerRpc(Vector3[] _pos, Quaternion[] _rot)
     {
         // Sedan uppdaterar vi då för klienten från servern
-        UpdateTransformClientRpc(_pos, _rot, _index);
+        UpdateTransformClientRpc(_pos, _rot);
     }
 
     // Den här headern innebär att vi skickar information från servern till klienten
     [ClientRpc]
-    private void UpdateTransformClientRpc(Vector3 _pos, Quaternion _rot, int _index)
+    private void UpdateTransformClientRpc(Vector3[] _pos, Quaternion[] _rot)
     {
         // Så ifall objektet inte tillhör ägaren, utan en ann klient så är den då ett represeterande objekt av den klienten
         // Då ändrar vi då dens positioner till informationen given av servern
-        if (!IsOwner && target_transform[_index] != null)
+        for (int _i = 0; _i < target_object.Length; _i++)
         {
-            target_position[_index] = _pos;
-            target_rotation[_index] = _rot;
+            if (!IsOwner && target_object[_i] != null)
+            {
+                target_position[_i] = _pos[_i];
+                target_rotation[_i] = _rot[_i];
+            }
         }
     }
 }
