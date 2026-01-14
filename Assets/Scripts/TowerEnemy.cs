77f01b1b6146f02a3e8ff693dd27f4b876012f06
UnavailableNameIsNotAvailable <154227444+UnavailableNameIsNotAvailable@users.noreply.github.com>
2026-01-14 10:13:32 +0100

    Tower enemy
    tower flower enemy

--- Assets/Scripts/TowerEnemy.cs
+++ Assets/Scripts/TowerEnemy.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 
-public class Kristall : MonoBehaviour
+public class TowerEnemy : MonoBehaviour
 {
-    // Script for rarer crystal item
 
     bool destroyState = false;
 
@@ -10,32 +9,46 @@ public class Kristall : MonoBehaviour
     float targetSize;
     float shrinkSpeed;
 
+    public GameObject projectile;
+    float countDown;
+    float coolDownTimer;
+
     void Start()
     {
         destroyState = false;
-        realSize = 2;
+        realSize = 3;
         targetSize = 0.1f;
         shrinkSpeed = 0.4f;
+        coolDownTimer = 1;
     }
 
     private void Update()
     {
+        Instantiate(projectile);
         if (destroyState)
         {
             realSize -= 1 * Time.deltaTime * shrinkSpeed;
             gameObject.transform.localScale = new Vector3(realSize, realSize, realSize);
             if (realSize < targetSize)
             {
-                CurrencyManager.crystalPoints += 2;
-                CurrencyManager.currencyPoints += 5;
-                Destroy(gameObject);//Adds extra crystal points and destroys object when small enough.
+                CurrencyManager.crystalPoints += 5;
+                CurrencyManager.currencyPoints += 14;
+                Destroy(gameObject);
             }
+            countDown = coolDownTimer;
         }
+        else if(countDown < 0)
+        {
+            Instantiate(projectile);
+            coolDownTimer = Random.Range(3, 7);
+            countDown = coolDownTimer;
+        }
+        countDown -= 1 * Time.deltaTime;
     }
 
     private void OnCollisionStay(Collision collisionlayer)
     {
-        if (collisionlayer.gameObject.tag == "Deposit")//Detect deposit and triggers destruction state.
+        if (collisionlayer.gameObject.tag == "Deposit")
         {
             destroyState = true;
         }
