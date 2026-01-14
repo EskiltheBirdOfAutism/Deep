d89f604c80a523fa7c5958c959259caf7cab5928
UnavailableNameIsNotAvailable <154227444+UnavailableNameIsNotAvailable@users.noreply.github.com>
2026-01-14 09:15:40 +0100

    Added currency manager
    Values:
    crystal points
    currency points

---
+++ Assets/Scripts/Currency Manager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CurrencyManager : MonoBehaviour
+{
+    //Manage
+    public static int crystalPoints;
+    public static int currencyPoints;
+
+    void Start()
+    {
+        crystalPoints = 0;
+        currencyPoints = 0;
+    }
+
+    void Update()
+    {
+        
+    }
+}
