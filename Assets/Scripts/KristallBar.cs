bf80bada4be8a0031adbc22e61be6f9e19391543
Sophiyax <sophiya.dhungana@elev.ga.lbs.se>
2026-01-14 22:09:12 +0100

    Upd skript
    La till en timer på 30 och 60 sek.
    La till kommentarer

--- Assets/Scripts/KristallBar.cs
+++ Assets/Scripts/KristallBar.cs
@@ -4,26 +4,42 @@ using UnityEngine.UI;
 
 public class KristallBar
 {
-    public Slider slider;
-    public float fillSpeed = 1.5f;
+    public Slider slider;  //UI mätaren 
+    public float fillSpeed = 1.5f; // Hur snabbt den fylls
 
-    float currentValue = 0f;
-    float targetValue = 0f;
+    float currentValue = 0f; // Vad baren är på just nu
+    float targetValue = 0f; //Hur mycket den ska fyllas upp till
 
-    int crystalsCollected = 0;
+    int crystalsCollected = 0; //Antal kristaller spelaren tagit 
 
+    float drainTimer = 0f; //Hur lång tid högheten ska räcka
+    float drainDuration = 0f; //30 eller 60 sek beroende på antal kristaller
     void Start()
     {
-        slider.value = 0f;
+        slider.value = 0f;  //Mätaren startas tom
     }
     void Update()
-    {
+    {   // Fyllning på baren blir smooth
+
         if (currentValue < targetValue)
         {
             currentValue += fillSpeed * Time.deltaTime;
-            currentValue = Mathf.Min(currentValue, targetValue);
-            slider.value = currentValue;
         }
+        //Timern räknas ner när baren fylls 
+        if (drainTimer > 0)
+        {
+            drainTimer -= Time.deltaTime;
+            float t = drainTimer / drainDuration;  //Hur mycket progress som ska vara kvar
+            targetValue = Mathf.Clamp01(t);
+        }
+        else
+        {  //När tiden är slut blir baren tom igen
+            targetValue = 0f;
+            crystalsCollected = 0;
+        }
+
+        currentValue = Mathf.MoveTowards(currentValue, targetValue, fillSpeed * Time.deltaTime);
+        slider.value = currentValue;
     }
 
     public void CollectCrystal()
@@ -31,10 +47,21 @@ public class KristallBar
         crystalsCollected++;
 
         if (crystalsCollected == 1)
+        {
             targetValue = 0.5f;
-
+            drainDuration = 30f; //30 sek tills baren blir tom
+            drainTimer = 30f;
+        }
         else if (crystalsCollected == 2)
+        {
             targetValue = 1f;
+            drainDuration = 60f; //60 sek tills baren blir tom
+            drainTimer = 60f;
+        }
+        
+
+        
+            
     }
 
 }
