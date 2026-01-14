e1f56e96bad74b83bd54daa55f3975fdf60e51b7
SiggeSolders <sigge.hallquistsolders@elev.ga.lbs.se>
2026-01-14 11:51:36 +0100

    Nya mindre rum

--- Assets/Scripts/AddFloors.cs
+++ Assets/Scripts/AddFloors.cs
@@ -10,13 +10,13 @@ public class AddFloors : MonoBehaviour
     {
         for (int i = 0; i < 5; i++)
         {
-            floorPosition = new Vector3(0, -i + room.transform.position.y - 16, 0);
+            floorPosition = new Vector3(room.transform.position.x - 4, -i + room.transform.position.y - 16, room.transform.position.z);
             Instantiate(bottomFloorPrefab, floorPosition, Quaternion.identity, room.transform);
 
         }
         for (int i = 0; i < 4; i++)
         {
-            floorPosition = new Vector3(0, i + room.transform.position.y + 1, 0);
+            floorPosition = new Vector3(room.transform.position.x - 4, i + room.transform.position.y + 1, room.transform.position.z);
             Instantiate(topFloorPrefab, floorPosition, Quaternion.identity, room.transform);
         }
     }
