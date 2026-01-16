f201a0523e3758fb0fbf979784401c7336bb0f0d
Bug-Schnugg <sigge.solders@icloud.com>
2026-01-16 11:05:35 +0100

    Flashlight, Empty Hand, Sound och Lightfix

--- Assets/Scripts/Player/CameraHolder.cs
+++ Assets/Scripts/Player/CameraHolder.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class CameraHolder : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform thirdPersonCamera; // Drag your actual camera here
+    public float defaultDistance = 8f; // How far the camera normally sits from this holder
+    public LayerMask collisionLayers;
 
-    // Update is called once per frame
-    void Update()
+
+    void LateUpdate()
     {
-        
+        // Raycast backwards from holder toward where camera sits
+        RaycastHit hit;
+        Vector3 direction = thirdPersonCamera.position - transform.position;
+
+        if (Physics.Raycast(transform.position, direction, out hit, defaultDistance, collisionLayers))
+        {
+            // Hit a wall - move camera to hit point with small offset
+            thirdPersonCamera.localPosition = new Vector3(-(hit.distance - 0.2f), 0, 0);
+        }
+        else
+        {
+            // No obstruction - use default distance
+            thirdPersonCamera.localPosition = new Vector3(-defaultDistance, 0,0);
+        }
     }
 }
